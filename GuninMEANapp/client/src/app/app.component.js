6afdfa3a0b1ae9df99d60d3cdab577151bd5d03d
Gunin Khanna <g3khanna@uwaterloo.ca>
2018-03-05 12:18:15 -0500

    TASK UI added

--- GuninMEANapp/client/src/app/app.component.js
+++ GuninMEANapp/client/src/app/app.component.js
@@ -15,8 +15,9 @@ var AppComponent = (function () {
     }
     AppComponent = __decorate([
         core_1.Component({
+            moduleId: module.id,
             selector: 'my-app',
-            template: "<tasks></tasks>",
+            templateUrl: "app.component.html",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- GuninMEANapp/client/src/app/app.component.ts
+++ GuninMEANapp/client/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<tasks></tasks>`,
+  templateUrl: `app.component.html`,
 })
 export class AppComponent { name = 'Angular'; }
