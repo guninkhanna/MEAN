69961f97149d949839d645cdfcf2f676dbd5d437
Gunin Khanna <g3khanna@uwaterloo.ca>
2018-03-05 12:29:36 -0500

    TASK service initialized

--- GuninMEANapp/client/src/app/app.module.ts
+++ GuninMEANapp/client/src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 
+import { HttpModule } from '@angular/http';
+
+
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpModule],
   declarations: [AppComponent, TasksComponent],
   bootstrap: [AppComponent]
 })
--- GuninMEANapp/client/src/app/components/tasks/tasks.component.ts
+++ GuninMEANapp/client/src/app/components/tasks/tasks.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
 
 @Component({
   moduleId: module.id,
   selector: 'tasks',
   templateUrl: `tasks.component.html`,
 })
-export class TasksComponent { name = 'Angular'; }
+
+
+export class TasksComponent {
+  constructor(private taskService: TaskService) {
+
+  }
+
+}
